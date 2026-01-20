9fd22e8b80ad881d627aa0f532e27e51a85c8b84
Mainak Chowdhury <mainak555@github.com>
2026-01-20 15:30:32 +0530

    HF data registration (Rubric 1)

---
+++ data-reg.py
@@ -0,0 +1,18 @@
+
+from huggingface_hub import login, HfApi
+from util import create_hf_repo
+import os
+
+hfApi = HfApi(token=os.getenv("HF_TOKEN"))
+hf_repo = os.getenv('HF_REPO')
+
+# Create HF Data Space
+create_hf_repo(hfApi, hf_repo, "dataset")
+
+# Upload tourism.csv to HF Data Space
+hfApi.upload_folder(
+    allow_patterns=["tourism.csv"],
+    folder_path="./data",
+    repo_type="dataset",
+    repo_id=hf_repo,
+)
