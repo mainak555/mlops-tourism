f56412d8426757945e894c7563ce5ad013127db9
Mainak Chowdhury <mainak555@github.com>
2026-01-23 01:56:16 +0530

    model selection wip

--- model_building/train_test_split.py
+++ model_building/train_test_split.py
@@ -29,7 +29,7 @@ X = df.drop(columns=['CustomerID', 'ProdTaken'])
 y = df['ProdTaken']
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42, stratify=y
+    X, y, test_size=0.2, random_state=40, stratify=y
 )
 
 # saving train/test split locally
