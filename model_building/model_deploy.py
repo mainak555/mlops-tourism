21feb3c82446be6234005c6b76a01887445335a1
Mainak Chowdhury <mainak555@github.com>
2026-01-23 19:06:51 +0530

    model deployed

--- model_building/model_deploy.py
+++ model_building/model_deploy.py
@@ -3,6 +3,9 @@ from model_train import get_train_test_split, evaluate
 from mlflow.tracking import MlflowClient
 from model_config import MODEL_CONFIG
 from huggingface_hub import HfApi
+from util import create_hf_repo
+from pathlib import Path
+import joblib
 import mlflow
 import json
 import os
@@ -63,4 +66,65 @@ model_dict = evaluate(f"{PIPELINE_RUN_ID}_final", "model_deploy", {
     model_name: MODEL_CONFIG[model_name] #only selected model
 }, X_train[top_k_features], y_train, X_test[top_k_features], y_test)
 
-print('done upto here')
+## model serialization ##
+bin_path = f"artifact/{MLFLOW_EXPERIMENT_NAME}.joblib"
+bin_name =f"{MLFLOW_EXPERIMENT_NAME}.joblib"
+version = f"v1.0.0-build.{PIPELINE_RUN_ID}"
+
+joblib.dump(model_dict[model_name], bin_path)
+
+## deploy to HF ##
+create_hf_repo(hfApi, HF_REPO, "model")
+hfApi.upload_file(
+    repo_id=HF_REPO,
+    repo_type="model",
+    path_in_repo=bin_name,
+    path_or_fileobj=bin_path,
+    commit_message=f"mlflow_run_id: {run_id}",
+) # type: ignore
+hfApi.create_tag(
+    repo_type="model",
+    repo_id=HF_REPO,
+    tag=version,
+)
+
+## tagging mlflow ##
+# log HF pointer mlflow artifact
+client.log_dict(run_id, {
+    "model_type": model_name,
+    "artifact": bin_name,
+    "hf_repo": HF_REPO,
+    "tag": version,
+}, artifact_file="hf_model")
+
+# register model in mlflow registry
+model_uri = f"runs:/{run_id}/hf_model"
+registered_model = client.create_registered_model(name=MLFLOW_EXPERIMENT_NAME)
+model_version = client.create_model_version(
+    description=f"HF model {version}",
+    name=MLFLOW_EXPERIMENT_NAME,
+    source=model_uri,
+    run_id=run_id,
+)
+
+# attach HF meta to registered model version
+client.set_model_version_tag(
+    version=model_version.version,
+    name=MLFLOW_EXPERIMENT_NAME,
+    key="hf_repo",
+    value=HF_REPO
+)
+client.set_model_version_tag(
+    version=model_version.version,
+    name=MLFLOW_EXPERIMENT_NAME,
+    key="version",
+    value=version
+)
+client.set_model_version_tag(
+    version=model_version.version,
+    name=MLFLOW_EXPERIMENT_NAME,
+    key="mlflow_run_id",
+    value=run_id
+)
+
+print(f"Registered model (version: {model_version.version}) linked to (mlflow run: {run_id}) and (HF tag {version})")
