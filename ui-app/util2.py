23bbda63744c5a5ebcb483ecfce9c128198a23c6
Mainak Chowdhury <mainak555@github.com>
2026-01-24 23:44:31 +0530

    model deployment completed

--- ui-app/util2.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-
-def num_features_selector(X):
-    return X.select_dtypes(include=np.number).columns.to_list()   
-def cat_features_selector(X):
-    return X.select_dtypes(include=['object', 'category']).columns.to_list() 
